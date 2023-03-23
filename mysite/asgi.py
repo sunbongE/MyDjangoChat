1b671375e4510fe1b88a58a493af9b189aa9510c
taehopark <qkrxogh7@gmail.com>
2023-03-23 18:34:03 +0900

    서버구현: 고정된 채팅방

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -9,19 +9,20 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter # 이거 추가한거..
+from channels.routing import ProtocolTypeRouter, URLRouter  # 이거 추가한거..
 from django.core.asgi import get_asgi_application
-import app.routing
+import app.routing, chat.routing
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 # 요처을 채널스가 먼저 받고
 django_asgi_app = get_asgi_application()
 
-#HTTP요청은 장고를 통해 처리하도록 세팅한다.
-application = ProtocolTypeRouter({
-
-    "http":django_asgi_app,
-    
-    "websocket" : URLRouter(
-        app.routing.websocket_urlpatterns
-    ),
-})
+# HTTP요청은 장고를 통해 처리하도록 세팅한다.
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": URLRouter(
+            app.routing.websocket_urlpatterns + chat.routing.websocket_urlpatterns
+        ),
+    }
+)
