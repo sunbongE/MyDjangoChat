9bca2ea2d8d1a01560d20b0f1573d56a2aa25b37
taehopark <qkrxogh7@gmail.com>
2023-04-04 23:52:10 +0900

    add_func:채팅방 삭제시 유저들에게 알림 후 페이지 이동 구현

--- chat/consumers.py
+++ chat/consumers.py
@@ -65,3 +65,7 @@ class ChatConsumer(JsonWebsocketConsumer):
                 "sender": message_dict["sender"],
             }
         )
+
+    def chat_room_deleted(self, message_dict):
+        custom_code = 4000
+        self.close(code=custom_code)
--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from channels.layers import get_channel_layer
+from django.db.models.signals import post_delete
+from asgiref.sync import async_to_sync
 
 
 # Create your models here.
@@ -23,3 +26,21 @@ class Room(models.Model):
     @staticmethod
     def make_chat_group_name(room=None, room_pk=None):
         return "chat-%s" % (room_pk or room.pk)
+
+
+def room__on_post_delete(instance, **kwargs):
+    # Consumer Instance밖에서 채팅방 그룹에 속한
+    # 모든 Consumer Instance들에게
+    # send message =>  chat.room.deletd
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        instance.chat_group_name,
+        {
+            "type": "chat.room.deleted",
+        },
+    )
+
+
+post_delete.connect(  # Room 삭제시 호출될 함수를 지정한다.
+    room__on_post_delete, sender=Room, dispatch_uid="room__on_post_delete"
+)
