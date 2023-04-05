580f9d9f0865cef74ec3a3fa166ec63950892e2c
taehopark <qkrxogh7@gmail.com>
2023-04-05 16:11:42 +0900

    웹소켓 CSRF보안 적영

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -13,6 +13,8 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
+
 
 # 앱에 있는 routing.py를 불러온다.
 import app.routing
@@ -25,10 +27,13 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(  # AuthMiddlewareStack: 쿠키/세션/인증 사용가능설정.
-            URLRouter(  # 불러온 routing.py의 주소들로 접근하면 웹속켓으로 관리한다고 명시한다.
-                app.routing.websocket_urlpatterns + chat.routing.websocket_urlpatterns
-            )
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(  # AuthMiddlewareStack: 쿠키/세션/인증 사용가능설정.
+                URLRouter(  # 불러온 routing.py의 주소들로 접근하면 웹속켓으로 관리한다고 명시한다.
+                    app.routing.websocket_urlpatterns
+                    + chat.routing.websocket_urlpatterns
+                )
+            ),
         ),
     }
 )
